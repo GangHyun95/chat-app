af2ebbb4094a9bc5f819170c0d76df017f18956b
허강현 <hgh6128@gmail.com>
2025-04-30 21:32:18 +0900

    회원가입, 로그인 페이지 UI 작업 및 기능 구현

---
+++ frontend/src/store/useAuthStore.ts
@@ -0,0 +1,87 @@
+import { create } from 'zustand';
+import { axiosInstance } from '../lib/axios';
+import toast from 'react-hot-toast';
+import { AxiosError } from 'axios';
+
+type AuthState = {
+    authUser: string | null;
+    isSigningUp: boolean;
+    isLoggingIn: boolean;
+    isUpdatingProfile: boolean;
+    isCheckingAuth: boolean;
+    checkAuth: () => Promise<void>;
+    signup: (data: formData) => Promise<void>;
+    login: (data: formData) => Promise<void>;
+    logout: () => Promise<void>;
+};
+
+type formData = {
+    fullName?: string;
+    email: string;
+    password: string;
+};
+
+export const useAuthStore = create<AuthState>((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get('/auth/check');
+            set({ authUser: res.data });
+        } catch (error) {
+            console.log('Error in checkAuth: ', error);
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post('/auth/signup', data);
+            set({ authUser: res.data });
+            toast.success('회원가입이 완료되었습니다!');
+        } catch (error) {
+            const err = error as AxiosError<{ message: string }>;
+            const errorMessage =
+                err.response?.data?.message || 'Unknown error occurred';
+            toast.error(errorMessage);
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post('/auth/login', data);
+            set({ authUser: res.data });
+            toast.success('로그인되었습니다.');
+        } catch (error) {
+            const err = error as AxiosError<{ message: string }>;
+            const errorMessage =
+                err.response?.data?.message || 'Unknown error occurred';
+            toast.error(errorMessage);
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post('/auth/logout');
+            set({ authUser: null });
+            toast.success('로그아웃되었습니다.');
+        } catch (error) {
+            const err = error as AxiosError<{ message: string }>;
+            const errorMessage =
+                err.response?.data?.message || 'Unknown error occurred';
+            toast.error(errorMessage);
+        }
+    },
+}));
