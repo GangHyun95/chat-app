58b670ed8a68275bf219016cfd5174a3b672b9e4
허강현 <hgh6128@gmail.com>
2025-05-01 03:21:17 +0900

    Settings 페이지 UI 작업 및 Theme 기능 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,14 @@ import LoginPage from './pages/LoginPage';
 import SettingsPage from './pages/SettingsPage';
 import ProfilePage from './pages/ProfilePage';
 import { useAuthStore } from './store/useAuthStore';
+import { useThemeStore } from './store/useThemeStore';
 import { useEffect } from 'react';
 import { Loader } from 'lucide-react';
 import { Toaster } from 'react-hot-toast';
 
 export default function App() {
     const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+    const { theme } = useThemeStore();
 
     useEffect(() => {
         checkAuth();
@@ -27,7 +29,7 @@ export default function App() {
         );
 
     return (
-        <div>
+        <div data-theme={theme}>
             <Navbar />
 
             <Routes>
@@ -51,7 +53,7 @@ export default function App() {
                     }
                 />
             </Routes>
-            <Toaster /> 
+            <Toaster />
         </div>
     );
 }
