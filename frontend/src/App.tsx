a09e4d741fd3337164277b06715655054d6071b1
허강현 <hgh6128@gmail.com>
2025-05-01 23:44:06 +0900

    로그아웃 오류 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -47,7 +47,7 @@ export default function App() {
                 <Route
                     path='/profile'
                     element={
-                        authUser ? <ProfilePage /> : <Navigate to='login' />
+                        authUser ? <ProfilePage /> : <Navigate to='/login' />
                     }
                 />
             </Routes>
